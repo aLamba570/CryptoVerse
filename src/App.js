534b02e256426808be2a19fc9423346075d36fe8
Ankush <alamba570@gmail.com>
2023-12-26 13:47:06 +0530

    #issue 1: routing Issue status: fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
 import './App.css';
+import Navbar from './components/Navbar';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
+import HomePage from './components/HomePage';
 
-function App() {
+const { Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <div className="navbar">
+          <Navbar />
+        </div>
+
+        <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+
+        <div className='footer'>
+          {/* Footer content */}
+        </div>
+        
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
