f4974069997a056806f1d88cb6b5b864918f0627
Ankush <alamba570@gmail.com>
2023-12-26 15:14:45 +0530

    minor change

--- src/api/cryptoApi.js
+++ src/api/cryptoApi.js
@@ -12,13 +12,14 @@ const createRequest = (url) => ({url, headers: cryptoApiHeaders});
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints : (builder) =>({
-        getCryptos:builder.query({
-            query : () => createRequest('./coins')
-        })
-    })
-});
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest(`./coins?limit=${count}`),
+      }),
+    }),
+  });
+  
 
 export const {
     useGetCryptosQuery,
