968de299c0b7b52c357916b3d07b0f5692df38e3
Ankush <alamba570@gmail.com>
2023-12-26 14:49:16 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout } from 'antd';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import './App.css';
 import Navbar from './components/Navbar';
 import Exchanges from './components/Exchanges';
@@ -19,9 +19,12 @@ const App = () => {
           <Navbar />
         </div>
 
+        
+
         <div className='main'>
           <Layout>
             <div className='routes'>
+            <Content style={{ marginLeft: '270px' }}>
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/exchanges" element={<Exchanges />} />
@@ -29,12 +32,23 @@ const App = () => {
                 <Route path="/crypto/:coinId" element={<CryptoDetails />} />
                 <Route path="/news" element={<News />} />
               </Routes>
+            </Content>
             </div>
           </Layout>
         </div>
 
         <div className='footer'>
-          {/* Footer content */}
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+          <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
         </div>
         
       </Layout>
