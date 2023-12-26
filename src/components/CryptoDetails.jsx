51e198d35ebea974c906c06e9d292f9960bc498a
Ankush <alamba570@gmail.com>
2023-12-26 13:47:19 +0530

    minor fixes

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            <h1>Crypto Details</h1>
+            {/* Add your component code here */}
+        </div>
+    );
+};
+
+export default CryptoDetails;
